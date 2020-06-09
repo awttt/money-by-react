33c14ace1576218ea695a063ee054cceaffdef9d
awttt <350944364@qq.com>
2020-06-10 00:38:22 +0800

    Money.tsx 备注

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -28,7 +28,25 @@ margin: 4px 12px;
 `;
 
 const NotesSection = styled.section`
+background: #f5f5f5;
+padding: 10px 16px;
+font-size: 14px;
+>label{
+display: flex;
+align-items: center;
 
+>span{
+margin-right: 16px;
+white-space: nowrap;
+}
+>input{
+display: block;
+width: 100%;
+height: 72px;
+background: none;
+border: none;
+}
+}
 `;
 
 const CategorySection = styled.section`
@@ -62,7 +80,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
 
